67e78c500e656a8916e185929f7da3e8c6b8a9aa
Rohan <rohan@rohan.io>
2016-07-29 03:12:00 +0530

    Automatically paginate all story transitions

--- lib/tracker_api/endpoints/story_transition.rb
+++ lib/tracker_api/endpoints/story_transition.rb
@@ -8,13 +8,29 @@ module TrackerApi
       end
 
       def get(project_id, params={})
-        data = client.get("/projects/#{project_id}/story_transitions", params: params).body
+        transitions = []
+        paginate = true
 
-        puts data.first
+        while paginate
+          response = client.get("/projects/#{project_id}/story_transitions", params: params)
 
-        data.map do |transition|
-          Resources::StoryTransition.new({ client: client, project_id: project_id }.merge(transition))
+          data = response.body
+          data.each do |transition|
+            transitions.push(Resources::StoryTransition.new({ client: client, project_id: project_id }.merge(transition)))
+          end
+
+          total = response.headers["X-Tracker-Pagination-Total"].to_i
+          offset = response.headers["X-Tracker-Pagination-Offset"].to_i
+          returned = response.headers["X-Tracker-Pagination-Returned"].to_i
+          if total > (offset + returned)
+            paginate = true
+            params[:offset] = offset + returned
+          else
+            paginate = false
+          end
         end
+
+        transitions
       end
 
     end
